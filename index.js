fe8d2ae1946e4c519202b7cda04d8a0644c2bf7c
Emma Egstad <emma@egstad.com>
2022-04-05 12:51:31 -0500

    created queue

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Stack
 class Stack {
   constructor() {
     this.stack = [];
@@ -29,3 +30,40 @@ console.log(stack.pop()); // 'llama'
 console.log(stack.peek()); // 'fox'
 console.log(stack.pop()); // 'fox'
 console.log(stack.pop()); // null
+
+//Queue
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(item) {
+    return this.queue.unshift(item);
+  }
+
+  dequeue() {
+    return this.queue.pop();
+  }
+
+  hasNext() {
+    if (this.queue[this.queue.length - 1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue.dequeue()); // 'fox'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue('llama');
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // false
+console.log(queue.dequeue()); // null
