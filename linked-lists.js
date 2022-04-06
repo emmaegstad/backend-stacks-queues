089b537a5ca595962b2ed0edb5bdfc9f5e40fc0c
Emma Egstad <emma@egstad.com>
2022-04-05 19:28:44 -0500

    creating add method for binary tree

--- linked-lists.js
+++ linked-lists.js
@@ -1,17 +1,32 @@
 class LinkedListNode {
   constructor(data) {
-    this.#data = data;
-    this.#next = null;
+    this.data = data;
+    this.next = null;
   }
 
   add(node) {
     //check for exisiting #next
-    if (this.#next) {
+    if (this.next) {
       //if there is no #next, add node
-      this.#next = node;
+      this.next = node;
     } else {
       //if there is, pass node to exisiting #next recursively
-      this.#next.add(node);
+      this.next.add(node);
+    }
+  }
+
+  getList() {
+    //should return a string with list of values in order
+    const list = '';
+    //add this to string
+    list + this.data;
+    //check for next
+    if (this.next) {
+      //if next exists, call getList on it
+      this.getList(this.next);
+    } else {
+      //if there is not a next, return string
+      return list;
     }
   }
 }
