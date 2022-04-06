7423a34b19119b96ba5dec1db182036c2da1c162
Emma Egstad <emma@egstad.com>
2022-04-05 20:03:42 -0500

    final push

--- binary-node.js
+++ binary-node.js
@@ -24,19 +24,31 @@ class BinaryTreeNode {
 }
 
 class PersonTreeNode {
-    constructor(person) {
-      this.value = person.name;
-      this.person = person;
-      this.left = null;
-      this.right = null;
-    }
-  
-    add(node) {
-      // implemented as in previous challenge
-    }
-  
-    findPerson(name) {
-      // Implement me!
-    }
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
   }
-  ```
+
+  add(value) {
+    //check edge case for incoming value matching this.value
+    if (value === this.value) return false;
+    //check case of incoming value being greater than this.value
+    //if so, pass value to the right and call add method
+    if (value > this.value) {
+      if (this.right) return this.right.add(value);
+      this.right = new BinaryTreeNode(value);
+      //otherwise, pass value to the left and call add method
+    } else {
+      if (this.left) return this.left.add(value);
+      this.left = new BinaryTreeNode(value);
+    }
+    //upon successful placement, return true
+    return true;
+  }
+
+  findPerson(name) {
+    // i do not know how to do this
+  }
+}
